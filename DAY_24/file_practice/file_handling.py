fde9f3900a2e08a2b560894fdc9376f2c15ee74e
Abhinav <mishraabhinav445@gmail.com>
2026-09-01 14:15:44 +0000

    DAY 24 file handling and upgraded snake game completed

---
+++ DAY_24/file_practice/file_handling.py
@@ -0,0 +1,9 @@
+with open("") as file:
+    contents = file.read()
+    print(contents)
+
+# with open("file.txt", mode="a") as file:
+#     file.write("\nThis is a new text.")
+
+# with open("new_file.txt", mode="w") as file:
+#     file.write("This is written in a new file created by file_handling.py")
